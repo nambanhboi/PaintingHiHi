d497793fc373aafa0b829cddcb2a0ac913029b3c
dinhhai <dinhthihai130903@gmail.com>
2023-06-27 21:44:25 +0700

    hai xinh dep

--- painting/static/ckeditor/config.js
+++ painting/static/ckeditor/config.js
@@ -26,20 +26,10 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about' }
 	];
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
+
 	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-	 // Define changes to default configuration here. For example:
-	 config.language = 'fr';
-	 // config.uiColor = '#AADC6E';
 	 config.htmlEncodeOutput = false;
 	 config.entities = false;
 	 config.entities_latin = false;
-	 config.ForceSimpleAmpersand = true;
+	//  config.ForceSimpleAmpersand = true;
 };
